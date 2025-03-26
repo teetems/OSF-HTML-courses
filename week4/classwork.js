6d5ae58bd748788ab1660f942cc4029d6036df19
adesola <temitopeadesola98@gmail.com>
2025-03-26 14:24:12 +0100

    third and fourth week

---
+++ week4/classwork.js
@@ -0,0 +1,14 @@
+B= 20;
+D=30;
+E=B+D;
+
+
+
+console.log(B+D)
+console.log(D-B)
+console.log(B*D)
+console.log(D/B)
+console.log(B**D)
+console.log(B++)
+console.log(B%D)
+
---
+++ week4/introjavascript/variable.js
@@ -0,0 +1,25 @@
+const para=document.getElementById("demo").innerText= "Big boys in Town";
+        document.write("i am a winner")
+        document.write(4+6)
+        window.alert("hey accept to click")
+        window.print();
+        //declaring variables
+
+        A= 10;
+        C= "you are c++";
+        D= 30;
+      let  first_name = "sola"
+        X= A + D
+        myg4= 20
+        let myGender = ""
+        console.log(first_name)
+        console.log(para)
+
+        function osf(){
+          console.log(A)
+          let sex= "female"
+          console.log(sex)
+        }
+        
+        console.log(osf())
+        
